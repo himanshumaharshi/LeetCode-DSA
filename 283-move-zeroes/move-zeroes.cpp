e317d62307ea659fb8f80338b620ef5818aca9e5
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-02-29 11:08:58 +0530

    Time: 11 ms (92.67%) | Memory: 21.6 MB (24.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
+    // Brute force approach (worst case)
+    // void moveZeroes(vector<int>& nums) {
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         for (int j = i + 1; j < nums.size(); j++) {
+    //             if (nums[i] == 0)
+    //             swap(nums[i], nums[j]);
+    //         }
+    //     }
+    // }
+
+    // optimal approach
     void moveZeroes(vector<int>& nums) {
+        int j = 0;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] == 0)
-                swap(nums[i], nums[j]);
-            }
+            if (nums[i] != 0)
+                nums[j++] = nums[i];
+        }
+        for (; j < nums.size(); j++) {
+            nums[j] = 0;
         }
     }
 };
