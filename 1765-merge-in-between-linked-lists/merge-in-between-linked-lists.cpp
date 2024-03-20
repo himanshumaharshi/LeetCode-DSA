04d6b97f2897a9cdf34559eeca3d9dcd5f539397
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-03-20 19:11:01 +0530

    Time: 158 ms (96.58%) | Memory: 98.1 MB (44.80%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {        
+        // place a pointer at tail of list2
+        ListNode* tail2 = list2;
+        while (tail2->next != NULL)
+            tail2 = tail2->next;
+
+        // get starting position -> a
+        ListNode* start = list1;
+        for (int i = 1; i < a; i++) {
+            start = start->next;
+        }
+
+        // get ending position -> b
+        ListNode* end = list1;
+        for (int i = 0; i < b; i++) {
+            end = end->next;
+        }
+
+        // connect the starting node (a - 1 node) to head of list2
+        start->next = list2;
+
+        // connect tail of list 2 with next of end node
+        tail2->next = end->next;
+
+        // disconnect end node from list1
+        end->next = NULL;
+
+        cout << "start: " << start->val << endl;
+        cout << "end: " << end->val << endl;
+        return list1;
+    }
+};
