ce3034cf35d47a6d2cca7d43c1cd8b5087f4b368
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-03-20 21:53:52 +0530

    Time: 13 ms (46.00%) | Memory: 20.5 MB (55.48%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // array to store merged intervals
         vector<vector<int>> merged;
         int i = 0;
 
+        // iterate through intervals until finding an interval whose end is before the start of 'newInterval', then add each of these intervals to the 'merged' vector and increment 'i' by 1
         while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
             merged.push_back(intervals[i]);
             i++;
         }
 
+        // 1. iterate through intervals as long as there are intervals left and the start of the current interval is less than or equal to the end of 'newInterval'
         while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            // 2. Update 'newInterval' to merge the current interval with 'newInterval' by taking the minimum start and maximum end values, then increment 'i'
             newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])};
             i++;
         }
+        // push newInterval in merged array
         merged.push_back(newInterval);
 
+        // continue iterating through the remaining intervals, adding each of them to the 'merged' vector and incrementing 'i'.
         while (i < intervals.size()) {
             merged.push_back(intervals[i]);
             i++;
