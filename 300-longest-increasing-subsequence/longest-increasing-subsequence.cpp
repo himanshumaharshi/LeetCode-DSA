4c1fb703ea44c17c58da2e561bdc5afcb837e74c
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-03-29 23:00:31 +0530

    Time: 189 ms (31.17%) | Memory: 13.1 MB (37.92%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -64,6 +64,51 @@ public:
         return dp[0][0];
     }
 
+    int spaceOptimized(vector<int>& nums){
+        vector<int> currRow(nums.size() + 1, 0);
+        vector<int> nextRow(nums.size() + 1, 0);
+        int n = nums.size();
+        
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+                // include
+                int include = 0;
+                if (prev == -1 || nums[curr] > nums[prev]) { // subsequence array is empty 
+                    include = 1 + nextRow[curr + 1];
+                }
+
+                // exclude
+                int exclude = 0 + nextRow[prev + 1];
+
+                currRow[prev + 1] = max(include, exclude);
+            }
+            nextRow = currRow;
+        }
+
+        return nextRow[0];
+    }
+
+    int optimalSolution(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        vector<int> ans;
+        ans.push_back(nums[0]);
+
+        for (int i = 1; i < nums.size(); i++) {
+            // include
+            if (nums[i] > ans.back()) {
+            ans.push_back(nums[i]);}
+            else {
+                // overwrite
+                // find index of just bigger element
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
         int prev = -1;
@@ -72,17 +117,21 @@ public:
         // return solveUsingRecursion(nums, curr, prev);
 
         // -------- Dynamic Programming (Top Down Approach / Memoization) --------
-        // T.C ---> O(n * m), S.C ---> O(n * m) (recursive stack + dp array)
+        // T.C ---> O(n²), S.C ---> O(n² + n²) (n²)
         // step 1: create dp array
         // vector<vector<int>> dp(nums.size(), vector<int>(nums.size() + 1, -1));
         // return memoizationSolve(nums, curr, prev, dp);
 
         // -------- Dynamic Programming (Bottom Up Approach / Tabulation) -------- 
-        // T.C ---> O(n * m), S.C ---> O(n * m)
-        return tabulationSolve(nums);
+        // T.C ---> O(n²), S.C ---> O(n²)
+        // return tabulationSolve(nums);
 
         // -------- Space Optimized Approach -------- 
-        // T.C ---> O(n * m), S.C ---> O(2m) = O(m)
-        // return spaceOptimized(nums);
+        // T.C ---> O(n²), S.C ---> O(n)
+        return spaceOptimized(nums);
+
+        // -------- Space Optimized Approach -------- 
+        // T.C ---> O(n log n), S.C ---> O(n)
+        return optimalSolution(nums);
     }
 };
