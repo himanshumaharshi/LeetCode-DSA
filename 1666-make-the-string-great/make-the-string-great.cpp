62efe3a2ae4de25f19d2ff40e31a54e79e8bd9d2
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-04-05 18:59:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (11.17%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stack;
+        
+        for (char c : s) {
+            if (!stack.empty() && std::abs(c - stack.top()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        std::string result;
+        while (!stack.empty()) {
+            result = stack.top() + result;
+            stack.pop();
+        }
+        
+        return result;
+    }
+};
