f5712a6d7a0ccec9d1af913661e3ed0de180c0cb
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-05-17 20:35:57 +0530

    Time: 15 ms (23.72%) | Memory: 21.1 MB (98.58%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -6,35 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-private:
-    TreeNode* solve(TreeNode* root,int target){
-        if(root==NULL){
-            return NULL;
-        }
-        
-        root->left=solve(root->left,target);
-        root->right=solve(root->right,target);
-        if(root->left==NULL && root->right==NULL){
-            if((root->val) == target){
-                return NULL;
-            }
-            else{
-                return root;
-            }
-        }
-        return root;
-        
-    } 
 public:
-    TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        if(root->val ==target && root->right==NULL && root->left==NULL ){
+    TreeNode* dfs(TreeNode* root, int target) {
+        if (root == NULL) {
             return NULL;
         }
-        return solve(root,target);
-        
+
+        root->left = dfs(root->left, target);
+
+        root->right = dfs(root->right, target);
+
+        if (root->left == NULL && root->right == NULL && root->val == target) {
+            return NULL;
+        }
+
+        return root;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+       return dfs(root, target);
     }
 };
