9148c00e21e3e24eac5bc3e57d89640e6f67f5e4
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-05-27 15:30:38 +0530

    Time: 17 ms (59.41%) | Memory: 26.7 MB (70.73%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int n = nums.size() / 2;
+    // naive approach (brute force)
+    int bruteForce(vector<int>& nums, int n) {
         for (int i = 0; i < nums.size(); i++) {
             int freq = 0;
             for (int j = 0; j < nums.size(); j++) {
@@ -9,10 +9,36 @@ public:
                     freq++;
                 }
             }
-            if (freq > n) {
+            if (freq > n / 2) {
                 return nums[i];
             }
         }
-        return 0;
+        return -1;
+    }
+
+    //  better approach from brute force using hashmaps
+    int solveUsingHashmap(vector<int>& nums, int n) {
+        map<int, int> mp;
+        // store the frequency of all the elements in the map
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+        }
+
+        // now find the element with frequency > n / 2
+        for (auto ele: mp) {
+            if (ele.second > n / 2) 
+                return ele.first;
+        }
+        return -1;
+    }
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        //  brute force T.C = O(2N)
+        // int ansBruteForce = bruteForce(nums, n);
+
+        // optimised approach T.C = O(N)
+        int ansOptimised = solveUsingHashmap(nums, n);
+        return ansOptimised;
     }
 };
