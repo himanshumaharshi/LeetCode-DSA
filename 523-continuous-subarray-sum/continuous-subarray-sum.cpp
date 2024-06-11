581f9b384fe117b497a433eb9c64fa7abb54740f
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-06-11 18:46:45 +0530

    Time: 191 ms (35.73%) | Memory: 134.8 MB (30.29%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int prefixMod = 0;
-        unordered_map<int, int> modSeen;
-        modSeen[0] = -1;
-
+        unordered_map<int, int> mp;
+        mp[0] = -1;
         for (int i = 0; i < nums.size(); i++) {
             prefixMod = (prefixMod + nums[i]) % k;
-            if (modSeen.find(prefixMod) != modSeen.end()) {
+            if (mp.find(prefixMod) != mp.end()) {
                 // ensures that the size of subarray is atleast 2
-                if (i - modSeen[prefixMod] > 1) {
-                    return 1;
+                if (i - mp[prefixMod] > 1) {
+                    return true;
                 }
             } else {
                 // mark the value of prefixMod with the current index.
-                modSeen[prefixMod] = i;
+                mp[prefixMod] = i;
             }
         }
-        return 0;
+        return false;
     }
 };
