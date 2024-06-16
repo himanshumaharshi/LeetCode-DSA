317dedb428fd1ff3ca5b6e605f4f098e285fb599
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-06-16 20:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (82.46%) - LeetSync

--- 330-patching-array/patching-array.cpp
+++ 330-patching-array/patching-array.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        long maxreach = 0;
+        // maxSum to store maximum sum that can be formed using the elements in
+        // the array
+        long maxSum = 0;
+        // patch to keep track of the number of patches
         int patch = 0;
         int i = 0;
 
-        while (maxreach < n) {
-            if (i < nums.size() &&
-                nums[i] <= maxreach + 1) // this is for if do not patch
-            {
-                maxreach += nums[i];
+        while (maxSum < n) {
+            if (i < nums.size() && nums[i] <= maxSum + 1) {
+                // If the current element in the array is less than or equal to the maxSum,
+                // we can extend the range of sums up to maxSum + nums[i]
+                // and update maxSum and i accordingly 
+                maxSum += nums[i];
                 i++;
-            } else // this for patch
-            {
-                maxreach += (maxreach + 1);
+            } else {
+                // If the current element in the array is greater than maxSum,
+                // that means we have found a gap in our range of sums.
+                // To fill this gap, we need to add maxSum + 1 in maxSum which
+                // is and also  increment the patch count.
+                maxSum += maxSum + 1;
                 patch++;
             }
         }
