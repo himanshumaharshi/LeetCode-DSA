fd8f6ad2f88b88432d3879c0a463bb2834eae997
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-06-26 14:53:56 +0530

    Time: 137 ms (27.79%) | Memory: 81.1 MB (13.85%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
-    typedef pair<int, int> P;
+    // Approach-1 (Using sliding window + heap)
+    // T.C : O(nlogn)
+    // S.C : O(n)
+    //  typedef pair<int, int> P;
+    //  int longestSubarray(vector<int>& nums, int limit) {
+    //      int n = nums.size();
+    //      priority_queue<P> maxPq;
+    //      priority_queue<P, vector<P>, greater<P>> minPq;
+
+    //     int i = 0;
+    //     int j = 0;
+    //     int maxLength = 0;
+
+    //     while (j < n) {
+    //         maxPq.push({nums[j], j});
+    //         minPq.push({nums[j], j});
+
+    //         while (maxPq.top().first - minPq.top().first > limit) {
+    //             i = min(maxPq.top().second, minPq.top().second) + 1;
+
+    //             while (maxPq.top().second < i) {
+    //                 maxPq.pop();
+    //             }
+    //             while (minPq.top().second < i) {
+    //                 minPq.pop();
+    //             }
+    //         }
+
+    //         // Update maxLength with the length of the current valid window
+    //         maxLength = max(maxLength, j - i + 1);
+    //         j++;
+    //     }
+
+    //     return maxLength;
+    // }
+    // Approach-2 (Using sliding window + multiset)
+    // T.C : O(nlogn)
+    // S.C : O(n)
     int longestSubarray(vector<int>& nums, int limit) {
         int n = nums.size();
-        priority_queue<P> maxPq;
-        priority_queue<P, vector<P>, greater<P>> minPq;
 
+        multiset<int> st;
         int i = 0;
         int j = 0;
         int maxLength = 0;
 
         while (j < n) {
-            maxPq.push({nums[j], j});
-            minPq.push({nums[j], j});
+            st.insert(nums[j]);
 
-            while (maxPq.top().first - minPq.top().first > limit) {
-                i = min(maxPq.top().second, minPq.top().second) + 1;
-
-                while (maxPq.top().second < i) {
-                    maxPq.pop();
-                }
-                while (minPq.top().second < i) {
-                    minPq.pop();
-                }
+            while (*st.rbegin() - *st.begin() > limit) {
+                st.erase(st.find(nums[i]));
+                ++i;
             }
 
-            // Update maxLength with the length of the current valid window
             maxLength = max(maxLength, j - i + 1);
             j++;
         }
