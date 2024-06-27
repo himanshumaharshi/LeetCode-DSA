45a19a1dcb221707fdfe89bd0ff72de1318fca38
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-06-27 16:43:42 +0530

    Time: 116 ms (96.51%) | Memory: 70.9 MB (54.65%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        // Get the first edge, represented as a vector {a, b}.
-        vector<int> first  = edges[0];
-        // Get the second edge, represented as a vector {c, a}.
-        vector<int> second = edges[1];
+        // // Get the first edge, represented as a vector {a, b}.
+        // vector<int> first  = edges[0];
+        // // Get the second edge, represented as a vector {c, a}.
+        // vector<int> second = edges[1];
 
-        // Check if the first element of the first edge is present in the second edge.
-        if(first[0] == second[0] || first[0] == second[1]) {
-            // If it is, return the first element of the first edge.
-            return first[0];
-        }
+        // // Check if the first element of the first edge is present in the
+        // second edge. if(first[0] == second[0] || first[0] == second[1]) {
+        //     // If it is, return the first element of the first edge.
+        //     return first[0];
+        // }
 
-        // If the first element of the first edge is not present in the second edge,
-        // then the second element of the first edge must be the center.
-        return first[1];
+        // // If the first element of the first edge is not present in the second edge,
+        // // then the second element of the first edge must be the center.
+        // return first[1];
+
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
     }
 };
