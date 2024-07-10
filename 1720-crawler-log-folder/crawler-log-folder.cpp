b18fc5c027cddb97b60dc76c40514b31aea76dee
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-07-10 10:45:17 +0530

    Time: 9 ms (22.38%) | Memory: 13.9 MB (84.88%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -4,11 +4,8 @@ public:
         int operations = 0;
         for (int i = 0; i < logs.size(); ++i) {
             string str = logs[i];
-            if (str == "../" && operations >= 0) {
-                if (operations == 0)
-                    continue;
-                operations--;
-            }
+            if (str == "../")
+                operations = max(0, operations - 1);
             else if (str == "./")
                 continue;
             else {
