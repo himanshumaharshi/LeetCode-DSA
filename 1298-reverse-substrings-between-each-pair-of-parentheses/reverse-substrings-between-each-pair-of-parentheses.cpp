2adfdea06738c665fdc42ac90c4158bd468340e6
Himanshu Maharshi <87297632+himanshumaharshi@users.noreply.github.com>
2024-07-11 18:08:53 +0530

    Time: 3 ms (49.64%) | Memory: 7.9 MB (49.11%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         string result = "";
+        // Stack to store the positions of '('
         stack<int> st;
         for (int i = 0; i < s.length(); ++i) {
             if (s[i] == '(')
-                st.push(result.size());
+                st.push(result.size()); // Store the position of '(' in the result string
             else if (s[i] == ')') {
-                // reverse
+                // Reverse the substring from the position of the last '(' to the current end of the result string
                 reverse(result.begin() + st.top(), result.end());
+                // Remove the position of the last '(' from the stack
                 st.pop();
-            }
-            else {
+            } else {
+                // Append current character to the result
                 result.push_back(s[i]);
             }
         }
